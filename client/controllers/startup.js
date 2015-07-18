deb8915af8e9744523cadd46c8b4e4ba6a41a5bf
Martin Pham <mdpham@uwaterloo.ca>
2015-07-18 13:41:24 -0400

    init commit

---
+++ client/controllers/startup.js
@@ -0,0 +1,25 @@
+Meteor.startup(function () {
+  config = {
+    client_id: "9d3700f41a6e7c052108742a6d661971",
+    username: "phamartin", //user_id: 49699208
+    api: "http://api.soundcloud.com"
+  };
+
+  // Initialize SC connection
+  SC.initialize({
+    // client_id: SoundCloud dev id
+    client_id: config.client_id,
+    redirect_uri: ""
+  });
+
+  //Get my user information
+  $.get("http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/"+config.username+"&client_id="+config.client_id,
+    function(user){
+      console.log(user);
+      //Get user
+      $.get(config.api+"/users/"+user.id+"/playlists?client_id="+config.client_id, 
+        function(data){
+          console.log(data);
+      })
+  });
+});
