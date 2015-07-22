f4385773c1b8dee592383d4c9d5a71fd86fc3f2f
Martin Pham <mdpham@uwaterloo.ca>
2015-07-22 00:57:20 -0400

    page layout and controller

--- client/layout/home/homeCtrl.js
+++ client/layout/home/homeCtrl.js
@@ -2,5 +2,13 @@ gruve.controller("homeCtrl",
 	["$scope","$meteor",
 	function($scope, $meteor){
 		console.log("homecontroller");
+		$scope.test = "TEST";
+
+		$meteor.autorun($scope, function(){
+			$meteor.subscribe("songs").then(function(){
+				$scope.songs = $meteor.collection(Songs);
+				console.log("songs:", $scope.songs);
+			})
+		})
 	}
 ])
