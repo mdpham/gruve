f702ece81df6700f41357dff3355e9a3ad0bba1f
Martin Pham <mdpham@uwaterloo.ca>
2015-07-24 00:30:27 -0400

    time display while playing

--- client/layout/home/homeCtrl.js
+++ client/layout/home/homeCtrl.js
@@ -1,6 +1,6 @@
 gruve.controller("homeCtrl",
-	["$scope","$meteor",
-	function($scope, $meteor){
+	["$scope","$meteor","$http",
+	function($scope, $meteor, $http){
 		console.log("homecontroller");
 
 		$scope.playlists = $meteor.collection(Playlists);
@@ -9,11 +9,29 @@ gruve.controller("homeCtrl",
 				$scope.playlists = $meteor.collection(Playlists);
 				// console.log("playlists:", $scope.playlists);
 			});
-
-			$('div.track-card.card .image').dimmer({
-			  on: 'hover'
-			});
 		});
+
+		var state = {
+			playing: false
+		};
+
+		var players = {
+			all: ".ui.basic.fullscreen.modal.player.playing",
+			playing: ".ui.basic.fullscreen.modal.player.playing",
+			idle: ".ui.basic.fullscreen.modal.player.idle"
+		};
+
+		$scope.togglePlayer = function(){
+			console.log(players.playing, $(".ui.basic.fullscreen.modal.player.playing"));
+			$(".ui.basic.fullscreen.modal.player").modal("hide");
+			if (state.playing) {
+				$(players.playing)
+					.modal("setting", "closable", false).modal("show");
+			} else {
+				$(players.idle)
+					.modal("setting", "closable", false).modal("show");
+			}
+		};
 
 		$scope.togglePlaylists = function(){
 			$("div.playlist-container").transition("slide down");
@@ -34,24 +52,58 @@ gruve.controller("homeCtrl",
 			$scope.togglePlaylists();
 		};
 
-		$scope.playState = "pick a song";
-		$scope.playTrack = function(id){
-			SC.get("/tracks/"+id, function(track){
-				console.log(track);
-				//Reset sounds
-				soundManager.stopAll();
-				soundManager.destroySound("current");
-				//Play
-				soundManager.createSound({
-					id: "current",
-					url: track.stream_url + "?client_id="+config.client_id,
-					whileplaying: function(){
-						console.log(this);
-						console.log(this.position/this.durationEstimate);
-					}
-				}).play();
 
-			})
+		var toPositionTime = function(ms) {
+			var minutes = ms/1000/60 << 0;
+			var seconds = ((ms/1000) % 60) << 0;
+			console.log(minutes, seconds);
+			seconds = (seconds < 10 ? "0": "") + seconds;
+			return minutes + ":" + seconds;
+		};
+		var updateCurrentTrack = function(track){
+			console.log(track);
+			track.duration = toPositionTime(track.duration);
+			$scope.currentTrack = track;
+			//Update artwork in player
+			$("img.current-artwork").attr("src", track.artwork_url);
+			$("img.current-waveform").attr("src", track.waveform_url);
+		};
+
+		$scope.selectTrack = function(id){
+			//SoundManager config for waveform
+			soundManager.setup({flashVersion: 9});
+
+			//Get track
+			var promise = $http.get(config.api.tracks+id+config.api.credentials);
+			promise
+				.then(function(result){
+					console.log(result)
+					var track = result.data;
+					console.log(track);
+					//Reset sounds
+					soundManager.stopAll();
+					soundManager.destroySound("current");
+					//Play
+					soundManager.createSound({
+						id: "current",
+						url: track.stream_url + "?client_id="+config.client_id,
+						whileplaying: function(){
+							//sm object
+							// console.log(this);
+							$(".current-track-progress .current-track-position").text(toPositionTime(this.position));
+						}
+					}).play();
+
+					//Update to show in player
+					updateCurrentTrack(track);
+					return track;
+				})
+				.then(function(track){
+					console.log(track, "asd");
+					$(players.playing).modal("setting", "closable", false).modal("show");
+				});
+
+
 		};
 
 		$scope.togglePlay = function(){
